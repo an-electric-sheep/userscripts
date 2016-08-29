68c1dc22cd51f8c8f4457254057e2ab40c363bc9
Electric Sheep <anon@invalid.test>
2016-08-29 12:18:01 +0200

    version bump

--- scripts/pixiv_infinite_scroll.user.js
+++ scripts/pixiv_infinite_scroll.user.js
@@ -9,7 +9,7 @@
 // @match       *://www.pixiv.net/bookmark_new_illust*
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jszip/2.4.0/jszip.js
 // @downloadURL https://github.com/an-electric-sheep/userscripts/raw/master/scripts/pixiv_infinite_scroll.user.js
-// @version     0.7.2
+// @version     0.7.3
 // @grant       GM_xmlhttpRequest
 // @run-at      document-start
 // @noframes
